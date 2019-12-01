96f2b25681da54497b405c54b15987e2165147a1
yjh <1057443070@qq.com>
2019-12-01 19:25:45 +0800

    query参数 与 params参数的区别

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@
 import ajax from './ajax'
 
 // 注： 如果后面的参数是以对象形式传入的参数, 称为这种对象形式的参数称为query参数,
-//             它们是在问号后面（?）， 如：http://localhost:3000/shops?latitude=40.10038&longitude=116.36867
+//     它们是在问号后面（?）， 如：http://localhost:3000/shops?latitude=40.10038&longitude=116.36867
 //
 // 而, 例如：http://localhost:3000/position/40.10038,116.36867， 参数在路径中，这种是params参数
 
